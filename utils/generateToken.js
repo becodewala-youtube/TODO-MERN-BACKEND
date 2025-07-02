e3550f1ba697e00ae69ac7044bb9f4ec2206f0a9
becodewala-youtube <kvikash74673@gmail.com>
2025-07-02 11:05:24 +0530

    same stite changed to None in token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res,userId) =>{
     res.cookie('jwt',token,{
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    sameSite: 'strict',
+    sameSite: 'None',
      // Helps prevent CSRF attacks
 })
 
